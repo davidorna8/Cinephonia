ed5cf18a413107ff20dd32f425dc72feb6a886fa
Eva Gómez <rosa23.312@gmail.com>
2023-03-02 14:30:29 +0100

    Spring boot initializer añadido
    Hemos juntado el proyecto anterior al de spring para tenerlo todo junto y trabajar sobre ello.

---
+++ cinephonia/src/main/java/com/example/cinephonia/CinephoniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinephonia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephoniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinephoniaApplication.class, args);
+	}
+
+}
---
+++ cinephonia/src/test/java/com/example/cinephonia/CinephoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinephonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinephoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
