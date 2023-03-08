9cccfa7168bef31e4a2a224fc0de71d46aa2720f
David <davidorna8@gmail.com>
2023-03-08 14:46:17 +0100

    Register and Log in Buttons

--- cinephonia/src/main/java/com/example/cinephonia/User.java
+++ cinephonia/src/main/java/com/example/cinephonia/User.java
@@ -1,5 +1,77 @@
 package com.example.cinephonia;
 
 public class User {
+    String name;
+    String surname;
+    String username;
+    String age;
+    String password;
+    String email;
+    String region;
 
+    public User(String name, String surname, String username, String age, String password, String email, String region) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.age = age;
+        this.password = password;
+        this.email = email;
+        this.region = region;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
