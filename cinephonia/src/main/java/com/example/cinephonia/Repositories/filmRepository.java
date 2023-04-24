623652c1406f8c2d47a17d1e59797f9133787cbd
Eva Gómez <rosa23.312@gmail.com>
2023-04-24 14:05:50 +0200

    first queries

--- cinephonia/src/main/java/com/example/cinephonia/Repositories/filmRepository.java
+++ cinephonia/src/main/java/com/example/cinephonia/Repositories/filmRepository.java
@@ -1,7 +1,19 @@
 package com.example.cinephonia.Repositories;
 
+import com.example.cinephonia.Models.Cover;
 import com.example.cinephonia.Models.Film;
+import com.example.cinephonia.Models.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface filmRepository extends JpaRepository<Film,Long> {
+    List<Film> findFilmById(long id);
+    List<Film> findFilmByCover(Cover cover);
+    List<Film> findFilmByFilmUser(User user);
+    List<Film> findByGenre(String genre);
+    List<Film> findFirst10ByDirector(String director, Sort sort);
+    int countByGenre (String genre);
+    List<Film> queryByYearAndDirector(String year, String director);
 }
